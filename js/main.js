58f4ae3163215b10cc383724a944e374568a56f2
Luigi Fontana <luigifontana92@gmail.com>
2020-02-17 15:29:22 +0100

    Completamento terzo Esercizio

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ var color = prompt('Qual è il tuo colore preferito?');
 document.getElementById('output').innerHTML = name + surname + color + '20';
 */
 
+/* Secondo Esercizio
 var name = prompt('Inserisci il tuo Nome');
 // console.log(name);
 var surname = prompt('Inserisci il tuo Cognome');
@@ -18,3 +19,15 @@ var data = new Date();
 var currentYear = data.getFullYear();
 // console.log(currentYear);
 document.getElementById('output').innerHTML = name + surname + color + currentYear;
+*/
+
+var name = prompt('Inserisci il tuo Nome');
+// console.log(name);
+var surname = prompt('Inserisci il tuo Cognome');
+// console.log(surname);
+var color = prompt('Qual è il tuo colore preferito?');
+// console.log(color);
+var data = new Date();
+var currentYear = data.getFullYear();
+// console.log(currentYear);
+document.getElementById('output').innerHTML = name + ';' + surname + ';' + color + ';' + currentYear;
