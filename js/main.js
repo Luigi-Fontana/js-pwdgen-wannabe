7adc00f697fca05a995da9906f17a3f0ad7cf594
Luigi Fontana <luigifontana92@gmail.com>
2020-02-17 15:26:40 +0100

    Completamento secondo Esercizio

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* Primo Esercizio
 var name = prompt('Inserisci il tuo Nome');
 // console.log(name);
 var surname = prompt('Inserisci il tuo Cognome');
@@ -5,3 +6,15 @@ var surname = prompt('Inserisci il tuo Cognome');
 var color = prompt('Qual è il tuo colore preferito?');
 // console.log(color);
 document.getElementById('output').innerHTML = name + surname + color + '20';
+*/
+
+var name = prompt('Inserisci il tuo Nome');
+// console.log(name);
+var surname = prompt('Inserisci il tuo Cognome');
+// console.log(surname);
+var color = prompt('Qual è il tuo colore preferito?');
+// console.log(color);
+var data = new Date();
+var currentYear = data.getFullYear();
+// console.log(currentYear);
+document.getElementById('output').innerHTML = name + surname + color + currentYear;
